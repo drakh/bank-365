818be0cc2029d0aaac52fd2b18fdd057075bba5a
ihornorkin <ihornorkin@gmail.com>
2017-07-18 16:18:26 +0300

    Add responsive in article page

--- build/js/common.js
+++ build/js/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+(function () {$(document).ready(function() {
 
 	$(window).scroll(function() {
 		var o = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#related-posts").slick({
+/*	var slider = $("#related-posts").slick({
 		slidesToShow: 1.1,
 		slidesToScroll: 1,
 		infinity: false,
@@ -21,8 +21,16 @@ $(document).ready(function() {
 		dots: true,
 		arrows: true,
 		prevArrow: $(".arrows-prev"),
-		nextArrow: $(".arrows-next")
-	});
+		nextArrow: $(".arrows-next"),
+		responsive: [
+		{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+		]
+	});*/
 
 	$("input").on("input", function(o) {
 		"LABEL" == o.target.parentNode.tagName && $(this).val().length > 0 ? $(this).parent().addClass("focused") : $(this).parent().removeClass("focused")
@@ -62,20 +70,31 @@ $(document).ready(function() {
 	});
 
 	var cutWord = function cutWord() {
+		var oldText = [];
 		$('.blog-section .articles .content p').each(function(index, element) {
 			$(element).find('.read-more').remove();
-			var text = $(element).text();
-			var newText = text.split(' ');
-			newText = newText.slice(0, 7);
-			$(element).text(newText.join(' '));
-			$(element).append('<span class="read-more">....</span>')
+			oldText.push($(element).text());
+		});
+		$(window).resize(function() {
+			if ($(window).width() < 768) {
+				$('.blog-section .articles .content p').each(function(index, element) {
+					$(element).find('.read-more').remove();
+					var text = $(element).text();
+					var newText = text.split(' ');
+					newText = newText.slice(0, 7);
+					$(element).text(newText.join(' '));
+					$(element).append('<span class="read-more">....</span>')
+				});
+			} else {
+				$('.blog-section .articles .content p').each(function(index, element) {
+					$(element).text(oldText[index]);
+					$(element).append('<span class="read-more">....</span>')
+				});
+			}
 		});
 	}
 
 	cutWord();
 
-	$(window).resize(function() {
-		cutWord();
-	});
-
 });
+}());
